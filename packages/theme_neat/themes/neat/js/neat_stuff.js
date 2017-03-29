2038df0f083375d5daa7708de63e9e4b07a7daf5
kitecom <kitecom@gmail.com>
2017-03-29 14:41:12 +0100

    Box colours

--- packages/theme_neat/themes/neat/js/neat_stuff.js
+++ packages/theme_neat/themes/neat/js/neat_stuff.js
@@ -2,4 +2,29 @@ $('div.col-content').filter(function() {
   return $.trim($(this).html()) === ''
 }).remove()
 
-
+//Add custom colours tp columns
+$('div').each(function(){
+    if($(this).hasClass('light-grey')) {
+        $(this).parent('div').addClass("col-light-grey");
+    } 
+});
+$('div').each(function(){
+    if($(this).hasClass('mid-grey')) {
+        $(this).parent('div').addClass("col-mid-grey");
+    } 
+});
+$('div').each(function(){
+    if($(this).hasClass('dark-grey')) {
+        $(this).parent('div').addClass("col-dark-grey");
+    } 
+});
+$('div').each(function(){
+    if($(this).hasClass('light-green')) {
+        $(this).parent('div').addClass("col-light-green");
+    } 
+});
+$('div').each(function(){
+    if($(this).hasClass('mid-green')) {
+        $(this).parent('div').addClass("col-mid-green");
+    } 
+});
